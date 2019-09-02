dce68b1434554db8f752f06a3b40e912ba7b3f64
BalintAts <atsbalint@gmail.com>
2019-09-02 10:40:48 +0100

    x

--- player.py
+++ player.py
@@ -1,11 +1,22 @@
+from __future__ import print_function
 import random
+
 
 
 class Player:
     VERSION = "1.1 random "
 
     def betRequest(self, game_state):
+        log(game_state["players"])
+
         return random.randint(0, 100)
 
     def showdown(self, game_state):
         pass
+
+def log(message):
+    import sys
+    print(message, file=sys.stderr)
+
+if __name__ == '__main__':
+    log("fofisrfh")
